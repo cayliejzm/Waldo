3c8f44e4b0529aa2fb70a8d1b0406c1821f09046
cayliejzm <cayliemorck@gmail.com>
2016-06-07 09:58:57 -0700

    Fixed user authentication. Issue w adding Lost/Items still there.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :image, :address)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :address)
   end
 
   def authorise
